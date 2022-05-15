1fc1d84e8ae1b39414853e5918350db21c466a33
Kooreis <kooreis.official@gmail.com>
2022-05-15 11:00:00 -0500

    Added a script that prompts the user to enter a noun and stores the input in a variable.

--- Mad_Libs_Generator.cs
+++ Mad_Libs_Generator.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -7,17 +6,5 @@ class Program
     {
         Console.Write("Enter a noun: ");
         string noun = Console.ReadLine();
-
-        Console.Write("Enter a verb: ");
-        string verb = Console.ReadLine();
-
-        Console.Write("Enter an adjective: ");
-        string adjective = Console.ReadLine();
-
-        Console.Write("Enter an adverb: ");
-        string adverb = Console.ReadLine();
-
-        Console.WriteLine($"On a sunny day, a very {adjective} {noun} was walking down the street. The {noun} was {verb} {adverb}.");
     }
 }
-```
