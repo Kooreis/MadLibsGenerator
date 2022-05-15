bde5319c089b7f904e3b6973189b49fd6366c224
Kooreis <kooreis.official@gmail.com>
2022-05-15 11:00:00 -0500

    Initial setup of the MadLibsGenerator class and main method. A Scanner object is created to take user input. The user is prompted to enter a name, which is then stored in a variable.

--- Mad_Libs_Generator.java
+++ Mad_Libs_Generator.java
@@ -1,4 +1,3 @@
-```java
 import java.util.Scanner;
 
 public class MadLibsGenerator {
@@ -7,39 +6,5 @@ public class MadLibsGenerator {
 
         System.out.println("Enter a name: ");
         String name = scanner.nextLine();
-
-        System.out.println("Enter a verb: ");
-        String verb = scanner.nextLine();
-
-        System.out.println("Enter an adjective: ");
-        String adjective = scanner.nextLine();
-
-        System.out.println("Enter a noun: ");
-        String noun = scanner.nextLine();
-
-        System.out.println("Enter a place: ");
-        String place = scanner.nextLine();
-
-        System.out.println("Enter a number: ");
-        String number = scanner.nextLine();
-
-        System.out.println("Enter a color: ");
-        String color = scanner.nextLine();
-
-        System.out.println("Enter an animal: ");
-        String animal = scanner.nextLine();
-
-        System.out.println("Enter a food: ");
-        String food = scanner.nextLine();
-
-        System.out.println("Enter a profession: ");
-        String profession = scanner.nextLine();
-
-        System.out.println("Here is your Mad Libs story: ");
-        System.out.println(name + " was a " + profession + " who lived in " + place + ". One day, " + name +
-                " was " + verb + " when a " + color + " " + animal + " appeared. The " + animal +
-                " was carrying a " + adjective + " " + noun + ". " + name + " decided to make a " + food +
-                " out of the " + noun + ". After " + number + " hours, the " + food + " was ready. And they all lived happily ever after!");
     }
 }
-```
