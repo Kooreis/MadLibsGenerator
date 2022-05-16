ce3b32ab32029ef2697ce3b92f507937fbec0d88
Kooreis <kooreis.official@gmail.com>
2022-05-16 12:00:00 -0500

    Added prompts for the user to enter a verb, an adjective, and a noun. These inputs are stored in their respective variables.

--- Mad_Libs_Generator.java
+++ Mad_Libs_Generator.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
+System.out.println("Enter a verb: ");
+        String verb = scanner.nextLine();
 
-public class MadLibsGenerator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an adjective: ");
+        String adjective = scanner.nextLine();
 
-        System.out.println("Enter a name: ");
-        String name = scanner.nextLine();
-    }
-}
+        System.out.println("Enter a noun: ");
+        String noun = scanner.nextLine();
